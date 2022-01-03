f04767ce5bdbbe2ee9a3e19541aefe6010d5a442
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-01-03 10:20:26 -0800

    Create 50-A  Domino Piling.java

---
+++ 50-A  Domino Piling.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class DominoPiling {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int m=sc.nextInt();
+        int n=sc.nextInt();
+        int x=m*n/2;
+        System.out.println(x);
+    }
+}
