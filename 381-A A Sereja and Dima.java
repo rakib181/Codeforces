7a6abf1eac804024f052671a19c8bd5765edbade
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-01-04 21:26:24 -0800

    Create 381-A A Sereja and Dima.java

---
+++ 381-A A Sereja and Dima.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SerejaDima {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int s=0,d=0,k=0,l=n-1;
+        int a[]=new int[n];
+        for (int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+
+        for (int j=0;j<n;j++) {
+            if (a[k] > a[l] && j % 2 == 0) {
+                s += a[k];
+                k++;
+                continue;
+            }
+            if (a[k] < a[l] && j % 2 == 0) {
+                s += a[l];
+                l--;
+                continue;
+            }
+            if (a[k] > a[l] && j % 2 != 0) {
+                d += a[k];
+                k++;
+                continue;
+            }
+            if (a[k] < a[l] && j % 2 != 0) {
+                d += a[l];
+                l--;
+                continue;
+            }
+            if (a[k] == a[l] && j % 2 != 0) {
+                d += a[k];
+                l--;
+                continue;
+            }
+                s += a[k];
+                break;
+        }
+        System.out.println(+s+" "+d);
+    }
+}
