31ad42f8373a7a48058bf211acee607209df2f38
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-01-12 04:45:34 -0800

    Create 510-A Fox and Snake.java

---
+++ 510-A Fox and Snake.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class FoxAndSnakes {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt(),m=sc.nextInt();
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=m;j++){
+                if(i%2!=0) {
+                    System.out.print("#");
+                }else{
+                    if (i%4!=0){
+                        if (j==m){
+                            System.out.print("#");
+                        }else
+                            System.out.print(".");
+                    }else
+                    {
+                        if(j==1){
+                            System.out.print("#");
+                        }else
+                            System.out.print(".");
+                    }
+                }
+
+            }
+            System.out.println();
+        }
+    }
+}
