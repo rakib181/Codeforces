dd2a178e4e476aff441fcb8fd026596ba0391922
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-01-13 01:30:53 -0800

    Create 630-A Again Twenty Five!.java

---
+++ 630-A Again Twenty Five!.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class AgainTwentyFive {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+       double n=sc.nextDouble();
+        System.out.println("25");
+    }
+}
