4c3f6a93bc2156ebe7654d49fe1afa15c91b90b2
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-01-27 04:05:42 +0600

    Create 1553-A Digits Sum.java

---
+++ 1553-A Digits Sum.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++){
+			int n=sc.nextInt();
+			System.out.println((n+1)/10);
+		}
+	}
+}
