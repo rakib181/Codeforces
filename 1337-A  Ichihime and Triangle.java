22afcef0b01e87222f81f5eb9277a04de38b0e39
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-02-05 15:56:45 +0600

    Create 1337-A  Ichihime and Triangle.java

---
+++ 1337-A  Ichihime and Triangle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class IchihimeAndTriangle {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++) {
+			int a=sc.nextInt(),b=sc.nextInt(),c=sc.nextInt(),d=sc.nextInt();
+			System.out.println(Math.max(a, b)+" "+(Math.max(b, c))+" "+(Math.min(c, d)));
+		}
+	}
+}
