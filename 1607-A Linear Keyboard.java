f319340fa79b0c7f9414e30a9fd1421d026e932b
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-02-15 16:09:03 +0600

    Create 1607-A Linear Keyboard.java

---
+++ 1607-A Linear Keyboard.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.util.ArrayList;
+
+public class LinearKeyboard {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int p = 0; p < t; p++) {
+            String s = sc.next(), s1 = sc.next();
+            int y = 0, k = 0;
+            List<Integer> list = new ArrayList<>();
+            for (int j = 0; j < s1.length(); j++) {
+                for (int i = 0; i < s.length(); i++) {
+                    if (s.charAt(i) == s1.charAt(j)) {
+                        list.add(i+1);
+                    }
+                }
+            }
+            for (int i = 0; i < list.size()-1; i++) {
+                k = list.get(i) - list.get(i + 1);
+                y += Math.abs(k);
+            }
+            System.out.println(y);
+        }
+    }
+}
