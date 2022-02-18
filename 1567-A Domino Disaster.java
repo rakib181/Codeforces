08e2dbe3071cfe35f521092b18b953b09ea231da
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-02-18 15:33:17 +0600

    Update 1567-A Domino Disaster.java

--- 1567-A Domino Disaster.java
+++ 1567-A Domino Disaster.java
@@ -21,3 +21,24 @@ public class DominoDisaster {
     }
 
 }
+/*import java.util.Scanner;
+
+public class DominoDisaster2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n=sc.nextInt();
+            StringBuilder sb = new StringBuilder(sc.next());
+            for (int i = 0; i < sb.length(); i++) {
+                if (sb.charAt(i) == 'U') {
+                    sb.setCharAt(i, 'D');
+                } else if (sb.charAt(i) == 'D') {
+                    sb.setCharAt(i, 'U');
+                }
+            }
+            System.out.println(sb);
+        }
+    }
+}*/
+
