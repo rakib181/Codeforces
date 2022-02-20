ba4ee9492e304aa9df266d7cd810de66663d292d
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-02-20 18:52:13 +0600

    Update 1426-A Floor Number.java

--- 1426-A Floor Number.java
+++ 1426-A Floor Number.java
@@ -8,12 +8,22 @@ public class FloorNumber {
             return Math.floor(((n - 3)/ x) + 2);
         }
     }
+    static double solve1(double n,double x){
+        double c=2,ans=1;
+        while (c<n){
+            c+=x;
+            ans++;
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
         while (t-->0){
             double n=sc.nextInt(),x=sc.nextInt();
-            System.out.println((int)solve(n,x));
+           // System.out.println((int)solve(n,x));
+            System.out.println((int)solve1(n,x));
         }
     }
 }
+
