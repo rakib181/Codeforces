8998e470b7081f4bfd911142594ff652e78089f8
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-02-23 09:59:51 +0600

    Update 1325-B  CopyCopyCopyCopyCopy.java

--- 1325-B  CopyCopyCopyCopyCopy.java
+++ 1325-B  CopyCopyCopyCopyCopy.java
@@ -1,23 +1,39 @@
+
 import java.util.*;
-
-public class FiveCopy {
-
+public class Main {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		int t=sc.nextInt();
-		while(t-->0) {
+		while(t-->0){
 			int n=sc.nextInt();
 			int[] arr=new int[n];
-			for(int i=0;i<n;i++) {
-				arr[i]=sc.nextInt();
-			}
 			Set<Integer>set=new TreeSet<>();
-			for(int i=0;i<n;i++) {
+			for(int i=0;i<n;i++){
+				arr[i]=sc.nextInt();
 				set.add(arr[i]);
 			}
 			System.out.println(set.size());
 		}
-
 	}
-
 }
+
+
+	
+		
+		
+		
+			
+			
+			
+				
+			
+			
+			
+
+			
+
+		
+
+	
+
+
