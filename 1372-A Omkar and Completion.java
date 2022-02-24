2f15b490f62197034fee421d7c2788677de5df34
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-02-24 19:42:00 +0600

    Create 1372-A Omkar and Completion.java

---
+++ 1372-A Omkar and Completion.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+			int n=sc.nextInt();
+			int[] arr=new int[n];
+			for(int i=0;i<n;i++){
+				arr[i]=2;
+			}
+			for(int i=0;i<n;i++){
+				System.out.print(arr[i]+" ");
+			}
+			System.out.println();
+		}
+	}
+}
