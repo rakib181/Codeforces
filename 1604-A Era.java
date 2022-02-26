f76320a5f0cdc948b7f099539d695f6138ac2e54
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-02-26 12:58:32 +0600

    Create 1604-A Era.java

---
+++ 1604-A Era.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+			int n=sc.nextInt(),c=0,x=0;
+			int[] arr=new int[n+1];
+			for(int i=1;i<=n;i++){
+				arr[i]=sc.nextInt();
+			}
+			for(int i=1;i<=n;i++){
+				c=Math.max(c,(arr[i]-i));
+			}
+			System.out.println(c);
+		}
+	}
+}
