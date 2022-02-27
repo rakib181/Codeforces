1087e6cec409a0b6d4bb85d3435e70f240c54d99
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-02-27 15:00:45 +0600

    Create 1537-B Bad Boy.java

---
+++ 1537-B Bad Boy.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+			long n=sc.nextLong(),m=sc.nextLong(),i=sc.nextLong(),j=sc.nextLong();
+			System.out.println("1"+" "+"1"+" "+n+" "+m);
+		}
+	}
+}
