1eb4369bb459e5bb896709f1fa719e7c2906be03
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-03-06 04:04:37 +0600

    Update 1574-A Regular Bracket Sequences.java

--- 1574-A Regular Bracket Sequences.java
+++ 1574-A Regular Bracket Sequences.java
@@ -2,7 +2,6 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-
 		int t=sc.nextInt();
 		while(t-->0){
 			int n=sc.nextInt();
