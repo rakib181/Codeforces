980c23572eb6863b11e536c81f871364f8e7baff
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-03-06 03:04:41 +0600

    Create 1574-A Regular Bracket Sequences.java

---
+++ 1574-A Regular Bracket Sequences.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		StringBuilder sb=new StringBuilder();
+		int t=sc.nextInt();
+		while(t-->0){
+			int n=sc.nextInt();
+			String s="";
+			for(int i=0;i<n;i++){
+				for(int x=0;x<i;x++){
+					s+='(';
+				}
+				for(int x=0;x<i;x++){
+					s+=')';
+				}
+				for(int x=0;x<n-i;x++){
+					s+='(';
+				}
+				for(int x=0;x<n-i;x++){
+					s+=')';
+				}
+				System.out.println(s);
+				s="";
+			}
+		}
+	}
+}
