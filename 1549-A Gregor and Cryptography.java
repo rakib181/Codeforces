30be3c2c98d77ea7ccfb07ae79ba75b03e492ede
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-03-06 11:55:41 +0600

    Create 1549-A Gregor and Cryptography.java

---
+++ 1549-A Gregor and Cryptography.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+			int p=sc.nextInt();
+			System.out.println("2"+" "+(p-1));
+		}
+	}
+}
