3f2a1c1397686d3b35e865e25310a2c5578066fa
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-03-06 12:24:32 +0600

    Create 1398-A Bad Triangle.java

---
+++ 1398-A Bad Triangle.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+			int n=sc.nextInt();
+			int[] arr=new int[n];
+			for(int i=0;i<n;i++){
+				arr[i]=sc.nextInt();
+			}
+			if((arr[0]+arr[1])<=arr[n-1]){
+				System.out.println("1"+" "+"2"+" "+n);
+			}else{
+				System.out.println("-1");
+			}
+		}
+	}
+}
