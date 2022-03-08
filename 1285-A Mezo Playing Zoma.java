af319361f223ad430f61710eeff3462e56afe2ff
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-03-08 19:07:46 +0600

    Create 1285-A Mezo Playing Zoma.java

---
+++ 1285-A Mezo Playing Zoma.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		StringBuilder sb=new StringBuilder(sc.next());
+		System.out.println(n+1);
+	}
+}
