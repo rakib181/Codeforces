3ea2e5765515e590154a4435dfd172dab3a5e742
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-03-09 21:30:29 +0600

    Create 1234-A Equalize Prices Again.java

---
+++ 1234-A Equalize Prices Again.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class EqualizePricesAgain {
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int t=sc.nextInt();
+	while(t-->0){
+		int n=sc.nextInt(),sum=0;
+		int[] arr=new int[n];
+		for(int i=0;i<n;i++){
+			arr[i]=sc.nextInt();
+			sum+=arr[i];
+		}
+		System.out.println((int)Math.ceil((double)sum/n));
+		}	
+	}
+}
