e3fcd83c66f4790ed16e9c58021df15691776d71
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-03-11 00:08:10 +0600

    Create 1651-A Playoff.java

---
+++ 1651-A Playoff.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Playoff {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0) {
+			int n=sc.nextInt();
+			System.out.println((1<<n)-1);
+		}
+
+	}
+
+}
