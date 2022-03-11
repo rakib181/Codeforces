c2342428fb15ba187916de3d04d50dfe062a7c54
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-03-11 12:19:08 +0600

    Create 119-A Epic Game.java

---
+++ 119-A Epic Game.java
@@ -0,0 +1,26 @@
+/*----->Hope Can Set You Free<-----*/
+import java.util.*;
+public class Main {
+	public static int GCD(int x,int n){
+		if(n==0){
+			return x;
+		}
+		return GCD(n,x%n);
+	}
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt(),b=sc.nextInt(),n=sc.nextInt();
+	while(true){
+		n-=GCD(a,n);
+		if(n==0){
+			System.out.println("0");
+			break;
+		}
+		n-=GCD(b,n);
+		if(n==0){
+			System.out.println("1");
+			break;
+		}
+	}
+	}
+}
