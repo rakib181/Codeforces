7d5c5b77c40c8b794e207bd1fc9533256313482b
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-03-15 01:23:50 +0600

    Create 1455-A Strange Functions.java

---
+++ 1455-A Strange Functions.java
@@ -0,0 +1,13 @@
+/*----->Hope Can Set You Free<-----*/
+import java.util.*;
+public class StrangeFunctions {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+         while(t-->0) {
+        	StringBuilder sb=new StringBuilder(sc.next());
+        	System.out.println(sb.length());
+         }
+	}
+}
