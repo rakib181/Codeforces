d8aa98f1fc769bcd8b4e422798126d4f8c97c28b
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-03-18 14:46:42 +0600

    Create 1312-A Two Regular Polygons.java

---
+++ 1312-A Two Regular Polygons.java
@@ -0,0 +1,12 @@
+/*----->Hope Can Set You Free<-----*/
+import java.util.*;
+public class TwoRegularPolygons {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+			int n=sc.nextInt(),m=sc.nextInt();
+	System.out.println((n%m)==0?"YES":"NO");
+		}
+	}
+}
