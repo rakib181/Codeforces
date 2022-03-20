8351899c925ecec23d3e921ae4d6742daed88d2d
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-03-20 23:12:36 +0600

    Create 1654-A Maximum Cake Tastiness.java

---
+++ 1654-A Maximum Cake Tastiness.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class MaximumCakeTastiness {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0) {
+			int n=sc.nextInt();
+			int[] arr=new int[n];
+			for(int i=0;i<n;i++) {
+				arr[i]=sc.nextInt();
+			}
+			Arrays.sort(arr);
+			System.out.println(arr[n-1]+arr[n-2]);
+		}
+	}
+}
