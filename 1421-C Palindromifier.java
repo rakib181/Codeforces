535c6715df339981672d567428211053c7e04947
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-04-29 23:25:47 +0600

    Create 1421-C Palindromifier.java

---
+++ 1421-C Palindromifier.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Palindromifier {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		System.out.println("3"+"\n"+"L"+" "+"2"+"\n"+"R"+" "+"2"+"\n"+"R"+" "+(2*s.length()-1));
+	}
+}
